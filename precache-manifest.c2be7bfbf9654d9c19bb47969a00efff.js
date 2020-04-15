c9aad6005795a18212bd492cc20bae34ec4b6875
royandi <royandinicolas@mail.ugm.ac.id>
2020-04-15 17:47:54 +0700

    Updates

---
+++ precache-manifest.c2be7bfbf9654d9c19bb47969a00efff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25a11169d0903fde6549598bddc3d193",
+    "url": "/utsweb3-15119/index.html"
+  },
+  {
+    "revision": "2b6497396db41a10c864",
+    "url": "/utsweb3-15119/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "457ad670b3f875ad801b",
+    "url": "/utsweb3-15119/static/js/2.b583d41b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/utsweb3-15119/static/js/2.b583d41b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b6497396db41a10c864",
+    "url": "/utsweb3-15119/static/js/main.bf43a030.chunk.js"
+  },
+  {
+    "revision": "7d06f28fc3229d845637",
+    "url": "/utsweb3-15119/static/js/runtime-main.aa30eac2.js"
+  }
+]);
